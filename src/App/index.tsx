3d8c4486b3db519e8d59ad7f91bf57af95fd561e
Alex-Zhurkin <lexus1812141@gmail.com>
2021-10-08 20:45:06 +0300

    подготовка сомпонентов

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,21 +1,34 @@
-import React from 'react'
-import {
-    Link,
-    Switch,
-    Route,
-} from 'react-router-dom'
-import { Pages } from 'Pages/Routes'
+import React,{useEffect} from 'react'
+// import {
+//     Link,
+//     Switch,
+//     Route,
+// } from 'react-router-dom'
+// import { Pages } from 'Pages/Routes'
 
 export function App() {
     require('./index.styl')
+    useEffect(()=> {
+        fetch('https://api.spacexdata.com/v4/launches/latest')
+            .then(response => response.json())
+            .then(result => 
+                console.log("result", result)
+                
+                // setState({data: result}));
+            // .catch((e) => {
+            //   console.log(e);
+            //   this.setState({data: result, isFetching: false, error: e }));
+            // }
+            );
+    },[])
 
     return <React.Fragment>
-        <div className="menu">
+        {/* <div className="menu">
             {Pages.map((page, index) => <Link to={page.link} key={index}>{page.title}</Link>)}
         </div>
 
         <Switch>
             {Pages.map((page, index) => <Route exact path={page.link} component={page.component} key={index} />)}
-        </Switch>
+        </Switch> */}
     </React.Fragment>
 }
--- src/Pages/Content/index.tsx
+++ src/Pages/Content/index.tsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import './index.styl'
+
+// console.log("zhopa");
 
 export function Content() {
-    require('./index.styl')
+    
+    
+    // require('./index.styl')
+    // this.state ={ data: {}, isFetching: true, error: null };
+    // const[state, setState]= useState(data:{})
+    useEffect(()=> {
+        fetch('https://api.spacexdata.com/v4/launches/latest')
+            .then(response => response.json())
+            .then(result => 
+                console.log("result", result)
+                
+                // setState({data: result}));
+            // .catch((e) => {
+            //   console.log(e);
+            //   this.setState({data: result, isFetching: false, error: e }));
+            // }
+            );
+    },[])
+
 
     return <section className="content">
         <h1>Content page</h1>
